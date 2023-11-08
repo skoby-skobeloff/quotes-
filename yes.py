a76d910d40b10bead148bebe097c2884f6af1ed0
Skoby <128781900+skoby-skobeloff@users.noreply.github.com>
2023-11-08 17:24:53 +0500

    Create yes.py

---
+++ yes.py
@@ -0,0 +1,12 @@
+quotes = [
+    "You must be the change you wish to see in the world. -Mahatma Gandhi",
+    "Spread love everywhere you go. Let no one ever come to you without leaving happier. -Mother Teresa",
+    "The only thing we have to fear is fear itself. -Franklin D. Roosevelt",
+    "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that. -Martin Luther King Jr.",
+    "Do one thing every day that scares you. -Eleanor Roosevelt",
+    "Well done is better than well said. -Benjamin Franklin",
+    "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart. -Helen Keller",
+    "It is during our darkest moments that we must focus to see the light. -Aristotle",
+    "Do not go where the path may lead, go instead where there is no path and leave a trail. -Ralph Waldo Emerson",
+    "Be yourself; everyone else is already taken. -Oscar Wilde"
+]
